71249d953e883678f44135c39b3cce1640ea4196
brettjschmidt <brettjschmidt@gmail.com>
2023-09-12 05:18:30 -0500

    heroku commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import http from "http";
 import path from "path";
 
-const app = express();
+var app = express();
+
 app.use(express.static(path.join(__dirname, "build")));
 
 const port = process.env.PORT || "8080";
 app.set("port", port);
+
 const server = http.createServer(app);
 server.listen(port);
